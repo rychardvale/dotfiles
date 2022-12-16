af3b861f907cdd81f06f2a5d1690a6193c7d92ec
Rychard Valle <rychard.vale@gmail.com>
2022-12-16 01:38:44 -0300

    tokyo highlight update

--- .config/nvim/lua/rvalle/options.lua
+++ .config/nvim/lua/rvalle/options.lua
@@ -1,10 +1,8 @@
 local opt = vim.opt
 local g = vim.g
 
--- opt.nu = true
 opt.number = true
 opt.relativenumber = true
-
 opt.expandtab = false
 opt.tabstop = 4
 opt.shiftwidth = 4
@@ -12,32 +10,26 @@ opt.smartindent = true
 opt.ignorecase = true
 opt.smarttab = true
 opt.clipboard = "unnamedplus"
-opt.guifont = { "JetBrainsMono Nerd Font:h11" }
 opt.signcolumn = "yes"
 opt.splitbelow = true
 opt.splitright = true
-
 opt.laststatus = 2
 opt.showtabline = 2
-
 opt.foldenable=false
 opt.foldmethod="expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldlevel = 99
-
+opt.writebackup = false
+opt.guifont = { "JetBrainsMono Nerd Font:h10" }
+opt.updatetime = 100
 opt.cursorline = true
-
-g.netrw_liststyle = 3
-
 opt.termguicolors = true
 opt.background = "dark"
 opt.showmode = false
 opt.showcmd = false
 opt.scrolloff = 10
-opt.sidescrolloff = 4
+opt.sidescrolloff = 10
 opt.wrap = false
 opt.swapfile = false
 
-g.mkdp_preview_options = {
-	disable_sync_scroll = 1,
-}
+g.netrw_liststyle = 3
--- .config/nvim/lua/rvalle/themes/tokyo.lua
+++ .config/nvim/lua/rvalle/themes/tokyo.lua
@@ -2,51 +2,15 @@ local ok_tokyooooo, tokyo = pcall(require, "tokyonight")
 if not ok_tokyooooo then
 	return
 end
---[[ local M = {}
-M.default = {
-  none = "NONE",
-  bg_dark = "#1f2335",
-  bg = "#24283b",
-  bg_highlight = "#292e42",
-  terminal_black = "#414868",
-  fg = "#c0caf5",
-  fg_dark = "#a9b1d6",
-  fg_gutter = "#3b4261",
-  dark3 = "#545c7e",
-  comment = "#565f89",
-  dark5 = "#737aa2",
-  blue0 = "#3d59a1",
-  blue = "#7aa2f7",
-  cyan = "#7dcfff",
-  blue1 = "#2ac3de",
-  blue2 = "#0db9d7",
-  blue5 = "#89ddff",
-  blue6 = "#b4f9f8",
-  blue7 = "#394b70",
-  magenta = "#bb9af7",
-  magenta2 = "#ff007c",
-  purple = "#9d7cd8",
-  orange = "#ff9e64",
-  yellow = "#e0af68",
-  green = "#9ece6a",
-  green1 = "#73daca",
-  green2 = "#41a6b5",
-  teal = "#1abc9c",
-  red = "#f7768e",
-  red1 = "#db4b4b",
-  git = { change = "#6183bb", add = "#449dab", delete = "#914c54" },
-  gitSigns = {
-    add = "#266d6a",
-    change = "#536c9e",
-    delete = "#b2555b",
-  },
-} ]]
+
 tokyo.setup({
 	style = "night",
 	terminal_colors = true,
 	on_highlights = function(hl, c)
 		local green = "#ABE9B3"
 		local pink = "#F28FAD"
+
+		-- Telescope shit
 		hl.TelescopeNormal = {
 			bg = c.bg_dark,
 			fg = c.fg_dark,
@@ -67,13 +31,17 @@ tokyo.setup({
 			fg = c.bg_dark,
 		}
 
+		-- Cursor line black
 		hl.CursorLine = {
-			bg = c.bg_dark
+			bg = c.bg_dark,
 		}
+
+		-- Lsp Sga stuff
 		hl.LspSagaAutoPreview = {
 			fg = c.bg_dark,
 			bg = c.bg_dark,
 		}
+
 		hl.LspSagaHoverBorder = {
 			bg = c.bg_dark,
 			fg = c.bg_dark,
@@ -82,6 +50,7 @@ tokyo.setup({
 		hl.FloatBorder = {
 			bg = c.bg_dark,
 		}
+
 		-- Finder
 		hl.LspSagaLspFinderBorder = {
 			fg = c.bg_dark,
@@ -92,7 +61,6 @@ tokyo.setup({
 			bg = c.bg_dark,
 		}
 		hl.FinderPreviewSearch = {
-			-- fg = c.magenta2,
 			bg = c.bg_dark,
 		}
 		hl.FinderParam = {
